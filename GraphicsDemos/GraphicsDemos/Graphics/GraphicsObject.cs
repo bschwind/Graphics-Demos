0e45d321f8a00530096de46b7d5a4fbe578d1185
Brian Schwind <brian.schwind@m33integrated.com>
2011-06-01 21:39:51 -0400

    Added a GraphicsObject interface

---
+++ GraphicsDemos/GraphicsDemos/Graphics/GraphicsObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GraphicsDemos.Graphics
+{
+    public interface IGraphicsObject
+    {
+        void Update(GameTime g);
+        void Draw(GameTime g);
+    }
+}
