67043865b6a0e4dbd025f5301f349c9a728dc067
Brian Schwind <brian.schwind@m33integrated.com>
2011-06-01 21:38:07 -0400

    Added a config file to keep static info on the app

---
+++ GraphicsDemos/GraphicsDemos/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphicsDemos
+{
+    public static class Config
+    {
+        public const int ScreenWidth = 1280;
+        public const int ScreenHeight = 720;
+    }
+}
