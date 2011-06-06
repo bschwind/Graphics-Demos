5d7c2f5e4fddee74d7448d6ded962d9cbaf4a83c
Brian Schwind <brian.schwind@m33integrated.com>
2011-06-05 20:06:05 -0400

    Documented PrimitiveBatch

--- GraphicsDemos/GraphicsDemos/Graphics/PrimitiveBatch.cs
+++ GraphicsDemos/GraphicsDemos/Graphics/PrimitiveBatch.cs
@@ -8,14 +8,24 @@ using GraphicsDemos.Cameras;
 
 namespace GraphicsDemos
 {
-    //Used to draw simple shapes on the graphics card
-    //Intended for graphics debugging
+    /// <summary>
+    /// Used to draw simple shapes on the graphics card. Intended for graphics debugging.
+    /// </summary>
     public class PrimitiveBatch
     {
         GraphicsDevice device;
         BasicEffect effect;
-        int maxVertsPerDrawReach = 65535 * 2;
-        int maxVertsPerDrawHiDef = 1048575 * 2;
+        
+        //XNA is multiplatform. Some platforms can only draw so many vertices at once,
+        //depending on the profile. These numbers come from the XNA documentation for
+        //The "Reach" and "HiDef" profiles.
+        //HiDef devices: PC, Xbox 360
+        //Reach devices: Windows Phone 7, Zune
+        //HiDef devices can run both HiDef and Reach profiles
+        const int maxVertsPerDrawReach = 65535 * 2;
+        const int maxVertsPerDrawHiDef = 1048575 * 2;
+        int maxVertsPerDraw;
+
         int vertCounter = 0;
         int vertsPerPrimitive;
         VertexPositionColor[] verts;
@@ -41,14 +51,21 @@ namespace GraphicsDemos
             //or hidef, depending on the profile
             if (device.GraphicsProfile == GraphicsProfile.Reach)
             {
-                verts = new VertexPositionColor[maxVertsPerDrawReach];
+                maxVertsPerDraw = maxVertsPerDrawReach;
             }
             else
             {
-                verts = new VertexPositionColor[maxVertsPerDrawHiDef];
+                maxVertsPerDraw = maxVertsPerDrawHiDef;
             }
+
+            verts = new VertexPositionColor[maxVertsPerDraw];
         }
 
+        /// <summary>
+        /// Begins the batch process for drawing 3D primitives
+        /// </summary>
+        /// <param name="primType">Only line list and triangle list are supported</param>
+        /// <param name="cam">The camera to render with</param>
         public void Begin(PrimitiveType primType, Camera cam)
         {
             if (hasBegun)
@@ -64,6 +81,11 @@ namespace GraphicsDemos
             hasBegun = true;
         }
 
+        /// <summary>
+        /// The simplest call for PrimitiveBatch. Adds a single vertex to be rendered to the batch.
+        /// If called twice with different vertices while using a line list, a line would be rendered.
+        /// </summary>
+        /// <param name="vpc"></param>
         public void AddVertex(VertexPositionColor vpc)
         {
             if (!hasBegun)
@@ -80,12 +102,25 @@ namespace GraphicsDemos
             vertCounter++;
         }
 
+        /// <summary>
+        /// Draws a line with a given color
+        /// </summary>
+        /// <param name="v1">The position of the first vertex</param>
+        /// <param name="v2">The position of the second vertex</param>
+        /// <param name="color">The color of the line</param>
         public void DrawLine(Vector3 v1, Vector3 v2, Color color)
         {
             AddVertex(new VertexPositionColor(v1, color));
             AddVertex(new VertexPositionColor(v2, color));
         }
 
+        /// <summary>
+        /// Draws a triangle at the given points, with the given color
+        /// </summary>
+        /// <param name="v1">The triangle's first vertex</param>
+        /// <param name="v2">The triangle's second vertex</param>
+        /// <param name="v3">The triangle's third vertex</param>
+        /// <param name="color">The color of the triangle</param>
         public void DrawTriangle(Vector3 v1, Vector3 v2, Vector3 v3, Color color)
         {
             AddVertex(new VertexPositionColor(v1, color));
@@ -96,6 +131,12 @@ namespace GraphicsDemos
             AddVertex(new VertexPositionColor(v1, color));
         }
 
+        /// <summary>
+        /// Draws a grid with specified width and depth, in the positive XZ plane
+        /// </summary>
+        /// <param name="width">The number of cells the grid should have along the X axis</param>
+        /// <param name="depth">The number of cells the grid should have along the Z axis</param>
+        /// <param name="color">The color of the grid</param>
         public void DrawXZGrid(int width, int depth, Color color)
         {
             for (int x = 0; x < width; x++)
@@ -114,6 +155,12 @@ namespace GraphicsDemos
             }
         }
 
+        /// <summary>
+        /// Draws a grid with specified width and height, in the positive XY plane
+        /// </summary>
+        /// <param name="width">The number of cells the grid should have along the X axis</param>
+        /// <param name="depth">The number of cells the grid should have along the Y axis</param>
+        /// <param name="color">The color of the grid</param>
         public void DrawXYGrid(int width, int height, Color color)
         {
             for (int x = 0; x < width; x++)
@@ -132,6 +179,13 @@ namespace GraphicsDemos
             }
         }
 
+        /// <summary>
+        /// Fills a triangle with a specified color. Must be called with triangle list as the primitive type.
+        /// </summary>
+        /// <param name="v1">The triangle's first vertex</param>
+        /// <param name="v2">The triangle's second vertex</param>
+        /// <param name="v3">The triangle's third vertex</param>
+        /// <param name="color">The color of the triangle</param>
         public void FillTriangle(Vector3 v1, Vector3 v2, Vector3 v3, Color color)
         {
             AddVertex(new VertexPositionColor(v1, color));
@@ -139,8 +193,15 @@ namespace GraphicsDemos
             AddVertex(new VertexPositionColor(v3, color));
         }
 
+        /// <summary>
+        /// Submits the current buffer to the graphics card for rendering
+        /// </summary>
         private void Flush()
         {
+            if (vertCounter <= 0)
+            {
+                return;
+            }
             int primitiveCount = vertCounter / vertsPerPrimitive;
 
             effect.View = currentCam.View;
@@ -152,6 +213,12 @@ namespace GraphicsDemos
             vertCounter = 0;
         }
 
+        /// <summary>
+        /// Given a vertex buffer and a primitive type, draws the contents of buffer.
+        /// </summary>
+        /// <param name="buffer">The vertex buffer to render</param>
+        /// <param name="primType">The primtitive type to use</param>
+        /// <param name="cam"></param>
         public void DrawVertexBuffer(VertexBuffer buffer, PrimitiveType primType, Camera cam)
         {
             device.SetVertexBuffer(buffer);
@@ -159,18 +226,21 @@ namespace GraphicsDemos
             effect.Projection = currentCam.Projection;
 
             effect.CurrentTechnique.Passes[0].Apply();
-            int passes = buffer.VertexCount / maxVertsPerDrawReach;
-            int remainder = buffer.VertexCount % maxVertsPerDrawReach;
+            int passes = buffer.VertexCount / maxVertsPerDraw;
+            int remainder = buffer.VertexCount % maxVertsPerDraw;
             int offset = 0;
             for (int i = 0; i < passes; i++)
             {
-                device.DrawPrimitives(primType, offset, maxVertsPerDrawReach / numVertsPerPrimitive(primType));
-                offset += maxVertsPerDrawReach;
+                device.DrawPrimitives(primType, offset, maxVertsPerDraw / numVertsPerPrimitive(primType));
+                offset += maxVertsPerDraw;
             }
 
             device.DrawPrimitives(primType, offset, remainder / numVertsPerPrimitive(primType));
         }
 
+        /// <summary>
+        /// Finalizes the batch call
+        /// </summary>
         public void End()
         {
             if (!hasBegun)
