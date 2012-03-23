f0a71d4cb25f9fcf8282bc9645e17f68fe83d5d0
Brian Schwind <brian.schwind@furman.edu>
2012-03-23 16:59:32 -0400

    Added some interactive code to the physics demo

--- GraphicsDemos/GraphicsDemos/Demos/Physics3DDemo.cs
+++ GraphicsDemos/GraphicsDemos/Demos/Physics3DDemo.cs
@@ -5,11 +5,13 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 using GraphicsToolkit.Graphics;
 using GraphicsToolkit.Graphics.SceneGraph;
 using GraphicsToolkit.Physics._3D;
 using GraphicsToolkit.Physics._3D.Partitions;
 using GraphicsToolkit.Physics._3D.Bodies;
+using GraphicsToolkit.Input;
 
 namespace GraphicsDemos.Demos
 {
@@ -19,6 +21,9 @@ namespace GraphicsDemos.Demos
         private FirstPersonCamera cam;
         private PhysicsEngine3D engine;
         private Mesh sphere;
+        float radius = 0.5f;
+        Vector3 mouseForceStart;
+        bool mouseHeldDown = false;
 
         public override void LoadContent(ContentManager content, GraphicsDevice g)
         {
@@ -29,14 +34,14 @@ namespace GraphicsDemos.Demos
             cam.Pos = new Vector3(3, 3, 13);
 
             MeshBuilder mb = new MeshBuilder(g);
-            sphere = mb.CreateSphere(1f, 10, 10);
+            sphere = mb.CreateSphere(1f, 3, 3);
 
             setupEngine();
         }
 
         private void setupEngine()
         {
-            engine = new PhysicsEngine3D(new BruteForcePartition3D());
+            engine = new PhysicsEngine3D();
 
             float boxSize = 10;
 
@@ -48,16 +53,48 @@ namespace GraphicsDemos.Demos
             engine.AddRigidBody(new PlaneBody(-Vector3.Right, new Vector3(boxSize, 0, 0)));
             engine.AddRigidBody(new PlaneBody(new Vector3(0, 0, -1), new Vector3(0, 0, boxSize)));
 
-            int bodyCount = 4;
+            int bodyCount = 5;
             for (int i = 0; i < bodyCount; i++)
             {
-                engine.AddRigidBody(new SphereBody(new Vector3((i*2) + 1, 7, 2), new Vector3(i, 0, 0), 1f, 1f));
+                engine.AddRigidBody(new SphereBody(new Vector3((i*2) + 1, 6, 2), new Vector3(i + 1f, 0, 0), 1f, radius));
+            }
+
+            for (int i = 0; i < bodyCount; i++)
+            {
+                engine.AddRigidBody(new SphereBody(new Vector3((i * 2) + 1, 8, 2.4f), new Vector3(i - 1f, 0, 0), 1f, radius));
             }
         }
 
         public override void Update(GameTime g)
         {
             cam.Update(g);
+
+            if (InputHandler.MouseState.LeftButton == ButtonState.Pressed && InputHandler.LastMouseState.LeftButton == ButtonState.Released)
+            {
+                mouseHeldDown = true;
+                mouseForceStart = cam.Pos;
+
+            }
+            else if (InputHandler.MouseState.LeftButton != ButtonState.Pressed)
+            {
+                mouseHeldDown = false;
+            }
+
+            if (InputHandler.MouseState.RightButton == ButtonState.Pressed)
+            {
+                engine.AddRigidBody(new SphereBody(cam.Pos, cam.Dir * 4f, 1f, radius));
+            }
+
+            Vector3 force = Vector3.Zero;
+            Vector3 mouseWorldPos = cam.Pos;
+            if (mouseHeldDown)
+            {
+                force = mouseWorldPos - mouseForceStart;
+                foreach (RigidBody3D rb in engine.GetBodies())
+                {
+                    rb.AddForce(force);
+                }
+            }
 
             float dt = (float)g.ElapsedGameTime.TotalSeconds;
             engine.Update(g);
@@ -77,7 +114,11 @@ namespace GraphicsDemos.Demos
 
             for (int i = 0; i < engine.GetBodies().Count; i++)
             {
-                primBatch.DrawMesh(sphere, Matrix.CreateTranslation(engine.GetBodies()[i].Pos), cam);
+                SphereBody sb = engine.GetBodies()[i] as SphereBody;
+                if(sb != null)
+                {
+                    primBatch.DrawMesh(sphere, Matrix.CreateScale(sb.Radius) * Matrix.CreateTranslation(engine.GetBodies()[i].Pos), cam);
+                }
             }
         }
     }
